f5963b112e5fbd57be412cf09205a445f865c190
cmihiranga099 <chamod.bsc.se02@edu.lnbti.lk>
2026-06-18 13:21:23 +0530

    GoldvaultBackendApplication

--- goldvault-backend/src/main/java/lk/goldvault/backend/GoldvaultBackendApplication.java
+++ goldvault-backend/src/main/java/lk/goldvault/backend/GoldvaultBackendApplication.java
@@ -2,12 +2,14 @@ package lk.goldvault.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableScheduling
 public class GoldvaultBackendApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(GoldvaultBackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(GoldvaultBackendApplication.class, args);
+    }
 }
