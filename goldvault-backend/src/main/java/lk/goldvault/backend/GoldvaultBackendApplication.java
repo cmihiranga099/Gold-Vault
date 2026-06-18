0ad8abf011a8ed6a39be296bb3709e78bd249c20
cmihiranga099 <chamod.bsc.se02@edu.lnbti.lk>
2026-06-18 13:17:03 +0530

    Gold Vault

---
+++ goldvault-backend/src/main/java/lk/goldvault/backend/GoldvaultBackendApplication.java
@@ -0,0 +1,13 @@
+package lk.goldvault.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldvaultBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldvaultBackendApplication.class, args);
+	}
+
+}
---
+++ goldvault-backend/src/test/java/lk/goldvault/backend/GoldvaultBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.goldvault.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldvaultBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
